187a45fc19ed5e16e8b071c3e2d06df14c6cf9d5
Antonio Milan Otero <antonio.milan_otero@maxlab.lu.se>
2015-10-23 17:56:34 +0200

    Deployment changes
    llrffacade/llrf.py
    ------------------
      - Read and Write methods available for dynamic attributes
      - LlrfDevice property added.
      - Removed dependencies on fandango and facadedevice
    
    scripts/Llrf
    ------------
      - A new, more pythonic launcher, for the device
    
    setup.py
    --------
      - New launcher added to the scripts
      - Version number increased
    
    setup.cfg
    ---------
      - Dependencies removed.

--- llrffacade/llrf.py
+++ llrffacade/llrf.py
@@ -1,161 +1,45 @@
+#!/usr/bin/env python
+
+###############################################################################
+#     LLRF Facade device server.
+#
+#     Copyright (C) 2015  Max IV Laboratory, Lund Sweden
+#
+#     This program is free software: you can redistribute it and/or modify
+#     it under the terms of the GNU General Public License as published by
+#     the Free Software Foundation, either version 3 of the License, or
+#     (at your option) any later version.
+#
+#     This program is distributed in the hope that it will be useful,
+#     but WITHOUT ANY WARRANTY; without even the implied warranty of
+#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#     GNU General Public License for more details.
+#
+#     You should have received a copy of the GNU General Public License
+#     along with this program.  If not, see [http://www.gnu.org/licenses/].
+###############################################################################
+
+"""This module contains the Facade device server for the LLRF.
+"""
+
+__all__ = ["Llrf", "run_device"]
+
+__docformat__ = 'restructuredtext'
+
 __author__ = 'antmil'
 
-from PyTango import DispLevel, Attr, DevDouble, DevState, DevVarStringArray, AttrWriteType, AttributeProxy, WAttribute
-from PyTango.server import device_property, command, attribute#, run
-from facadedevice import FacadeMeta, logical_attribute, proxy_attribute, Facade
+from PyTango import Attr, DevDouble, DevState, DevVarStringArray, AttrWriteType, AttrQuality
+from PyTango.server import device_property, Device, DeviceMeta, run
+
 from taurus import Attribute
 import taurus.core
-import PyTango
 
 
-# from PyTango import Util,
-
-class Llrf(Facade):
+class Llrf(Device):
     """
-    Tango Device server for a high level control of the llrf cavities
+    Tango Device server for a high level control of the llrf cavities.
     """
-    __metaclass__ = FacadeMeta
-
-    # Attributes
-
-    RawAmprefinaKW = proxy_attribute(
-        dtype=float,
-        device='LlrfDevice',
-        attr='AmpRefInA',
-        display_level=DispLevel.EXPERT,
-        doc=''
-    )
-    # RawPhrefinaKW = proxy_attribute(
-    #     dtype=float,
-    #     device='LlrfDevice',
-    #     attr='PhRefInA',
-    #     display_level=DispLevel.EXPERT,
-    #     doc=''
-    # )
-    # RawDiag_amprefaKW = proxy_attribute(
-    #     dtype=float,
-    #     device='LlrfDevice',
-    #     attr='AmpRefA',
-    #     display_level=DispLevel.EXPERT,
-    #     doc=''
-    # )
-    # RawDiag_phrefaKW = proxy_attribute(
-    #     dtype=float,
-    #     device='LlrfDevice',
-    #     attr='PhRefA',
-    #     display_level=DispLevel.EXPERT,
-    #     doc=''
-    # )
-    # RawDiag_ampcavloopsaKW = proxy_attribute(
-    #     dtype=float,
-    #     device='LlrfDevice',
-    #     attr='AmpCavLoopsA',
-    #     display_level=DispLevel.EXPERT,
-    #     doc=''
-    # )
-    # RawDiag_ampfwcavloopsaKW = proxy_attribute(
-    #     dtype=float,
-    #     device='LlrfDevice',
-    #     attr='AmpFwCavLoopsA',
-    #     display_level=DispLevel.EXPERT,
-    #     doc=''
-    # )
-    # RawDiag_phcavloopsaKW = proxy_attribute(
-    #     dtype=float,
-    #     device='LlrfDevice',
-    #     attr='PhCavLoopsA',
-    #     display_level=DispLevel.EXPERT,
-    #     doc=''
-    # )
-    # RawDiag_phfwcavloopsaKW = proxy_attribute(
-    #     dtype=float,
-    #     device='LlrfDevice',
-    #     attr='PhFwCavloopsA',
-    #     display_level=DispLevel.EXPERT,
-    #     doc=''
-    # )
-    # RawDiag_amprvcavaKW = proxy_attribute(
-    #     dtype=float,
-    #     device='LlrfDevice',
-    #     attr='AmpRvCavA',
-    #     display_level=DispLevel.EXPERT,
-    #     doc=''
-    # )
-    # RawDiag_phrvcavaKW = proxy_attribute(
-    #     dtype=float,
-    #     device='LlrfDevice',
-    #     attr='PhRvCavA',
-    #     display_level=DispLevel.EXPERT,
-    #     doc=''
-    # )
-
-    @logical_attribute(
-        dtype=float,
-        #device='LlrfDevice',
-        unit='KW',
-        doc=''
-    )
-    def AmpRefInAKW(self, data):
-        VALUE = data['RawAmprefinaKW']
-        new_value = eval(self.AmpRefInAKW_formula)
-        return new_value
-
-    AmpRefInAKW_formula = device_property(
-        dtype=str,
-        default_value='VALUE*2',
-        doc=''
-    )
-
-    # @logical_attribute(
-    #     dtype=float,
-    #     unit='KW',
-    #     doc=''
-    # )
-    # def PhRefInAKW(self, data):
-    #     VALUE = data['RawPhRefInAKW']
-    #     new_value = eval(self.PhRefInAKW_formula)
-    #     return new_value
-    #
-    # PhRefInAKW_formula = device_property(
-    #     dtype=str,
-    #     default_value='VALUE',
-    #     doc=''
-    # )
-
-    # diag_amprefaKW
-    # diag_phrefaKW
-    # diag_ampcavloopsaKW
-    # diag_ampfwcavloopsaKW
-    # diag_phcavloopsaKW
-    # diag_phfwcavloopsaKW
-    # diag_amprvcavaKW
-    # diag_phrvcavaKW
-
-    @command(dtype_in=str)
-    def CreateFloatAttribute(self, attr_name):
-        attr = Attr(attr_name, DevDouble)
-        # attr = attribute(label=attr_name,
-        #                  dtype=float,
-        #                  #display_level=DispLevel.OPERATOR,
-        #                  access=AttrWriteType.READ_WRITE,
-        #                  unit='',
-        #                  format='%6.2f',
-        #                  min_value=0, max_value=10,
-        #                  rel_change='0.1',
-        #                  polling_period=1000,
-        #                  # fget="get_KpA",
-        #                  # fset="set_KpA",
-        #                  doc=""
-        # )
-
-        self.add_attribute(attr, self.read_General, self.write_General)
-
-    def read_General(self, attr):
-        self.info_stream("Reading attribute %s", attr.get_name())
-        attr.set_value(99.99)
-
-    def write_General(self, attr):
-        self.info_stream("Writting attribute %s", attr.get_name())
+    __metaclass__ = DeviceMeta
 
     DynamicAttributes = device_property(
         dtype=DevVarStringArray,
@@ -163,32 +47,39 @@ class Llrf(Facade):
         doc='Example: Attr = (Attr, Formula)'
     )
 
+    LlrfDevice = device_property(
+        dtype=str,
+        default_value='',
+        doc='Device server for the llrf device to proxy'
+    )
+
     def delete_device(self):
-        Facade.delete_device(self)
+        Device.delete_device(self)
         for att in self.dyn_attrs_dict.keys():
             listener_name = att + 'Listener'
             self.dyn_attrs_dict[att][0].removeListener(self.__dict__[listener_name])
 
     def init_device(self):
-        Facade.init_device(self)
+        Device.init_device(self)
         try:
             self.dyn_attrs_dict = {}
-            self.extract_attributes()
+            self.extract_attributes_from_property()
             self.create_dyn_attributes()
         except Exception, e:
             self.set_state(DevState.FAULT)
             self.set_status(e.message)
 
-    def extract_attributes(self):
+    def extract_attributes_from_property(self):
         self.info_stream("Extracting dynamicAttributes ...")
         dyn_attrs = self.DynamicAttributes
-        #print dyn_attrs
-        for attr in dyn_attrs:
-            self.get_attributes_dict(attr)
-        self.info_stream("... DynamicAttributes Extracted")
 
-    def get_attributes_dict(self, attr):
-        self.info_stream("Creating dynamic attributes dict ...")
+        for attr in dyn_attrs:
+            attr_name, attr_info_list = self.extract_attribute_info(attr)
+            self.info_stream("... DynamicAttributes info extracted")
+            self.create_listeners(attr_name, attr_info_list)
+            self.info_stream("... Listeners created")
+
+    def extract_attribute_info(self, attr):
         self.info_stream('Attribute = %s' % attr)
 
         attr_info = attr.split('=')
@@ -198,21 +89,26 @@ class Llrf(Facade):
         self.info_stream('attr_name = %s' % attr_name)
 
         attr_info_list = attr_info[1].strip("(").strip(")").split(',')
+        return attr_name, attr_info_list
 
-        if len(attr_info_list) == 2:
-            attr_to_proxy, r_method = attr_info_list
+    def create_listeners(self, attr_name, attr_info_list):
+        self.info_stream("Creating Listeners ...")
 
-        elif len(attr_info_list) == 3:
-            attr_to_proxy, r_method, w_method = attr_info_list
-
-
-        # attr_formula = attr_info[1].strip("(").strip(")").split(',')[1]
-        # self.info_stream('attr_formula = %s' % attr_formula)
-        self.info_stream('r_method = %s' % r_method)
-
-        # attr_to_proxy = attr_info[1].strip("(").strip(")").split(',')[0]
+        attr_to_proxy = attr_info_list[0]
         full_attr_to_proxy = self.LlrfDevice + '/' + attr_to_proxy
         taurus_attr = Attribute(full_attr_to_proxy)
+
+        if len(attr_info_list) == 2:
+            r_method = attr_info_list[1]
+            self.dyn_attrs_dict[attr_name] = (taurus_attr, r_method)
+
+        elif len(attr_info_list) == 3:
+            r_method, w_method = attr_info_list[1::]
+            self.dyn_attrs_dict[attr_name] = (taurus_attr, r_method, w_method)
+            self.info_stream('w_method = %s' % w_method)
+
+        self.info_stream('r_method = %s' % r_method)
+
         listener_name = attr_name + 'Listener'
         self.__dict__[listener_name] = taurus.core.TaurusListener(None)
         self.__dict__[listener_name].eventReceived = lambda a, b, c: self._dyn_attr_event_received(a,
@@ -222,13 +118,7 @@ class Llrf(Facade):
                                                                                                    r_method )
         taurus_attr.addListener(self.__dict__[listener_name])
 
-        self.info_stream('attr_to_proxy = %s' %attr_to_proxy)
-
-        if len(attr_info_list) == 2:
-            self.dyn_attrs_dict[attr_name] = (taurus_attr, r_method)
-
-        elif len(attr_info_list) == 3:
-            self.dyn_attrs_dict[attr_name] = (taurus_attr, r_method, w_method)
+        self.info_stream('attr_to_proxy = %s' % attr_to_proxy)
 
     def _dyn_attr_event_received(self, tango_attr, event_type, event_value, attr_nane, r_method):
         if event_type in [taurus.core.TaurusEventType.Periodic, taurus.core.TaurusEventType.Change]:
@@ -236,18 +126,20 @@ class Llrf(Facade):
             new_value = eval(r_method)
             time = event_value.time.tv_sec + 1e-6*event_value.time.tv_usec
             self.push_change_event(attr_nane, new_value, time, event_value.quality)
+
         elif event_type in [taurus.core.TaurusEventType.Error]:
             v = tango_attr.getValueObj()
             time = v.time.tv_sec + 1e-6 * v.time.tv_usec
-            self.push_change_event(attr_nane, 0, time, PyTango.AttrQuality.ATTR_INVALID)
-
+            self.push_change_event(attr_nane, 0, time, AttrQuality.ATTR_INVALID)
 
     def create_dyn_attributes(self):
         self.info_stream("Creating dynamic attributes ...")
         for att in self.dyn_attrs_dict.keys():
             self.create_dyn_attribute(att)
+        self.info_stream("... Dynamic attributes created")
 
     def create_dyn_attribute(self, attr_name):
+        self.info_stream("Creating dynamic attribute: %s" % attr_name)
         if len(self.dyn_attrs_dict[attr_name]) == 2:
             attr = Attr(attr_name, DevDouble, AttrWriteType.READ)
             self.add_attribute(attr, self.read_dyn_attributes)
@@ -260,9 +152,8 @@ class Llrf(Facade):
 
     def read_dyn_attributes(self, attr):
         attr_name = attr.get_name()
-        self.info_stream("Reading attribute %s", attr_name)
-        #attr_proxy = Attribute(self.LlrfDevice + '/' + self.dyn_attrs_dict[attr_name][0])
-        #VALUE = attr_proxy.read().value
+        self.info_stream("Reading attribute: %s", attr_name)
+
         VALUE = self.dyn_attrs_dict[attr_name][0].getValueObj().value
         r_method = self.dyn_attrs_dict[attr_name][1]
         new_value = eval(r_method)
@@ -270,20 +161,19 @@ class Llrf(Facade):
 
     def write_dyn_attributes(self, attr):
         attr_name = attr.get_name()
-        self.info_stream("Writing attribute %s", attr_name)
+        self.info_stream("Writing attribute: %s", attr_name)
         data=[]
         attr.get_write_value(data)
         VALUE = data[0]
-        #attr_proxy = Attribute(self.LlrfDevice + '/' + self.dyn_attrs_dict[attr_name][0])
+
         attr_proxy = self.dyn_attrs_dict[attr_name][0]
         w_method = self.dyn_attrs_dict[attr_name][2]
         new_value = eval(w_method)
         attr_proxy.write(new_value)
 
 
-
+def run_device():
+    run([Llrf])
 
 if __name__ == '__main__':
-    Llrf.run_server()
-    # run()
-    #run({'Llrf': (Llrf, ws_rf_llrf_1)})
+    run_device()
